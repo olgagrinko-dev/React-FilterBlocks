89be3500d2b5dd6bd4aacf47471a119b03e7ed74
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-09-13 22:06:11 +0300

    vacancy

--- client/src/pages/VacancyPage/VacancyPage.jsx
+++ client/src/pages/VacancyPage/VacancyPage.jsx
@@ -1,18 +1,36 @@
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import array from '../../Storag/Storag.json';
+import style from './style.module.scss';
 
 function VacancyPage() {
     const { id } = useParams();
+    const [data, setData] = useState({})
 
     useEffect(() => {
         const result = array.filter((elem) => elem.id == id);
         console.log(result);
+        setData(result[0]);
     }, [])
 
     return (
         <div>
-            <h1>{id}</h1>
+            <div className={style.vacancy}>
+                <h1>{data.header}</h1>
+
+                <div className={style.info}>
+                    <h2>{data.salary}</h2>
+                    <p>{data.workday}</p>
+                </div>
+
+                <div className={style.city}>
+                    <div className={style.img}></div>
+                    <p>{data.city}</p>
+                </div>
+
+
+            </div>
+
         </div>
     )
 }
