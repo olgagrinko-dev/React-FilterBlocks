b7c31fb26808ebbd88972b99714126135ad0455f
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-09-13 21:18:07 +0300

    project

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -2,15 +2,12 @@ import { Input } from '@mantine/core';
 import { IconSearch } from '@tabler/icons-react';
 import style from './style.module.scss';
 import { Link } from 'react-router-dom';
+import array from '../../Storag/Storag.json';
 
-function HomePage() {
-    const array = [{ id: 1, heder: 'Менеджер-дизайнер', salary: 'з/п от 70000 rub', workday: 'Полный рабочий день', city: 'Новый Уренгой' },
-    { id: 2, heder: 'Ведущий графический дизайнер НЕ УДАЛЕННО', salary: 'з/п от 80000 rub', workday: 'Полный рабочий день', city: 'Москва' },
-    { id: 3, heder: 'Работник декорации, дизайнер (ТЦ Амбар)', salary: 'з/п 29000 rub', workday: 'Сменный график работы', city: 'Самара' },
-    { id: 4, heder: 'Менеджер-дизайнер', salary: 'з/п 55000 - 95000 rub', workday: 'Полный рабочий день', city: 'Тюмень' }];
+function HomePage() {   
 
     const result = array.map((elem) => <Link to={`/vacancy/${elem.id}`}> <div className={style.item}>
-        <h2>{elem.heder}</h2>
+        <h2>{elem.header}</h2>
         <div className={style.flex}>
             <p>{elem.salary}</p>
             <p>{elem.workday}</p>
@@ -20,7 +17,7 @@ function HomePage() {
             <p>{elem.city}</p>
         </div>
     </div> </Link>)
-    
+
     return (
         <div>
             <Input icon={<IconSearch />} placeholder="Введите название вакансии" />
--- client/src/pages/VacancyPage/VacancyPage.jsx
+++ client/src/pages/VacancyPage/VacancyPage.jsx
@@ -1,6 +1,19 @@
-function VacancyPage(){
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import array from '../../Storag/Storag.json';
+
+function VacancyPage() {
+    const { id } = useParams();
+
+    useEffect(() => {
+        const result = array.filter((elem) => elem.id == id);
+        console.log(result);
+    }, [])
+
     return (
-        <div></div>
+        <div>
+            <h1>{id}</h1>
+        </div>
     )
 }
 
